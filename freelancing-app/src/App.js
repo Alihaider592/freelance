53e350e1066bf4c0b5ff8b82c9ca82e32e36dae0
Malik Ali <alicoder592@gmail.com>
2026-01-03 12:01:51 +0500

    takilwind working test

---
+++ freelancing-app/src/App.js
@@ -0,0 +1,11 @@
+
+// src/App.js
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
