054e60c97021d4f1b8459655e704d88a8ba65ee6
Malik Ali <alicoder592@gmail.com>
2026-01-07 10:42:52 +0500

    how it work seciton created

---
+++ freelancing-app/src/components/how_it_work.jsx
@@ -0,0 +1,9 @@
+const howitwork = () =>{
+    return(
+        <div>
+            <h2>How It Works</h2>
+            <p>This is a simple explanation of how our service works.</p>
+        </div>
+    )
+}
+export default howitwork;
